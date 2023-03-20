495fb818dae80d525a1f660809df75f7c0c01706
+ <+>
2023-03-20 19:11:21 -0400

    My Math is terrible

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(journey){
+    return Math.abs(journey - 42)
+
+}
+
+function distanceFromHqInFeet(blocks){
+    return Math.abs(blocks - 42) * 264
+}
+
+function distanceTravelledInFeet(start, end){
+    return Math.abs(start - end) * 264
+
+}
+
+function calculatesFarePrice(start, end) {
+  if (distanceTravelledInFeet(start, end) <= 400){
+    return 0 } 
+    else if  (distanceTravelledInFeet(start, end) > 400 && distanceTravelledInFeet(start, end) < 2000){
+        return (distanceTravelledInFeet (start, end) - 400 ) * 0.02
+    }
+    else if (distanceTravelledInFeet(start, end) > 2000 && distanceTravelledInFeet(start, end) < 2500){
+        return 25
+    }
+    else if (distanceTravelledInFeet(start, end) >= 2500){
+        return "cannot travel that far"
+    }
+
+    
+    
+  }
